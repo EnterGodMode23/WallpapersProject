dc1681734d60bfde8dda70288e3b39f7e514c66c
AveMaria23 <demyn300@gmail.com>
2023-02-20 21:54:35 +0300

    splash screen

--- app/src/main/java/com/example/wallpapersproject/presentation/MainActivity.kt
+++ app/src/main/java/com/example/wallpapersproject/presentation/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : AppCompatActivity() {
     private val API_KEY = "33786499-5584879290479131b764ec16b&q=bird&image_type=photo"
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_WallpapersProject)
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
